f468862e2e2b2004d948aba19756246d6c59bb77
satyamjha2001 <satyamjha23112001@gmail.com>
2025-02-19 15:36:11 +0530

    ham-burger menu

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden"); // Show/Hide menu on click
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll("#mobile-menu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
+});
